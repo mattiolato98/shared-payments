c75006854bcb72e50b8ab680d0f62cdcc16c1d8b
gabrymattioli <gabrymattioli@gmail.com>
2023-05-31 16:36:30 +0200

    ui, auth, AuthActivity: added UI related methods, refactoring

--- app/src/main/java/com/example/turtle/ui/auth/BaseAuthFragment.kt
+++ app/src/main/java/com/example/turtle/ui/auth/BaseAuthFragment.kt
@@ -2,7 +2,9 @@ package com.example.turtle.ui.auth
 
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
@@ -11,11 +13,13 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.example.turtle.MainActivity
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.Identity
 import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.material.textfield.TextInputEditText
@@ -23,6 +27,11 @@ import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.util.concurrent.CancellationException
+
+const val TAG = "AUTH"
+
 
 abstract class BaseAuthFragment: Fragment() {
 
@@ -34,20 +43,14 @@ abstract class BaseAuthFragment: Fragment() {
     abstract var progressBar: ProgressBar
     abstract var googleButton: Button
 
-    val viewModel: AuthViewModel by viewModels()
-    private lateinit var oneTapSignUpLauncher: ActivityResultLauncher<IntentSenderRequest>
+    val viewModel: AuthViewModel by activityViewModels()
+    private lateinit var oneTapSignInLauncher: ActivityResultLauncher<IntentSenderRequest>
     private lateinit var oneTapClient: SignInClient
-    private lateinit var firebaseAuthUiClient: FirebaseAuthUiClient
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         oneTapClient = Identity.getSignInClient(requireActivity())
 
-        firebaseAuthUiClient = FirebaseAuthUiClient(
-            context = requireContext(),
-            oneTapClient = Identity.getSignInClient(requireContext())
-        )
-
-        oneTapSignUpLauncher = registerForActivityResult(
+        oneTapSignInLauncher = registerForActivityResult(
             ActivityResultContracts.StartIntentSenderForResult()
         ) { result ->
             if (result.resultCode == RESULT_OK && result.data != null) {
@@ -93,8 +96,8 @@ abstract class BaseAuthFragment: Fragment() {
     private fun launchGoogleSignIn() {
         showProgressBar()
         viewLifecycleOwner.lifecycleScope.launch {
-            val signInIntentSender = firebaseAuthUiClient.initGoogleSignIn()
-            oneTapSignUpLauncher.launch(
+            val signInIntentSender = initGoogleSignIn()
+            oneTapSignInLauncher.launch(
                 IntentSenderRequest.Builder(signInIntentSender?: return@launch).build()
             )
             hideProgressBar()
@@ -111,6 +114,35 @@ abstract class BaseAuthFragment: Fragment() {
 
     private fun hideProgressBar() {
         progressBar.visibility = View.INVISIBLE
+    }
+
+    private suspend fun initGoogleSignIn(): IntentSender? {
+        val result = try {
+            oneTapClient.beginSignIn(
+                buildSignInRequest()
+            ).await()
+        } catch (e: Exception) {
+            Log.e(TAG, e.message.toString())
+            if (e is CancellationException) throw e
+            null
+        }
+        return result?.pendingIntent?.intentSender
+    }
+
+    private fun buildSignInRequest(): BeginSignInRequest {
+        return BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(requireContext().getString(
+                        com.firebase.ui.auth.R.string.default_web_client_id)
+                    )
+                    // Show all accounts on the device.
+                    .setFilterByAuthorizedAccounts(false)
+                    .build()
+            )
+            .build()
     }
 }
 
