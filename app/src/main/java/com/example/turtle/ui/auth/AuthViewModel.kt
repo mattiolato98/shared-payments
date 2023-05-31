802eb09a07b58450a57e781e20052a0e45fe42d0
gabrymattioli <gabrymattioli@gmail.com>
2023-05-31 12:14:07 +0200

    ui, auth, AuthViewModel: added isLoading state update

--- app/src/main/java/com/example/turtle/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/turtle/ui/auth/AuthViewModel.kt
@@ -16,24 +16,34 @@ open class AuthViewModel: ViewModel() {
     val state = _state.asStateFlow()
 
     fun signInWithGoogle(googleCredentials: AuthCredential) = viewModelScope.launch {
+        onStartAuth()
         val result = authRepository.signInWithGoogle(googleCredentials)
         onAuthResult(result)
     }
 
     fun signInWithEmailAndPassword(email: String, password: String) = viewModelScope.launch {
+        onStartAuth()
         val result = authRepository.signInWithEmailAndPassword(email, password)
         onAuthResult(result)
     }
 
     fun signUpWithEmailAndPassword(email: String, password: String) = viewModelScope.launch {
+        onStartAuth()
         val result = authRepository.signUpWithEmailAndPassword(email, password)
         onAuthResult(result)
+    }
+
+    private fun onStartAuth() {
+        _state.update { it.copy(
+            isLoading = true
+        ) }
     }
 
     private fun onAuthResult(result: AuthResult) {
         with(result) {
             _state.update { it.copy(
-                isSignInSuccessful = data != null,
+                isUserLoggedIn = data != null,
+                isLoading = false,
                 emailError = emailErrorMessage,
                 passwordError = passwordErrorMessage,
                 genericFormError = genericFormErrorMessage
