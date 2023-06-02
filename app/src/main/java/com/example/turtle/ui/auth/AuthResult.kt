35776aa7e0a739c92ac6208acea293a32d3fa944
gabrymattioli <gabrymattioli@gmail.com>
2023-06-02 11:18:59 +0200

    refactoring

--- app/src/main/java/com/example/turtle/ui/auth/AuthResult.kt
+++ app/src/main/java/com/example/turtle/ui/auth/AuthResult.kt
@@ -1,11 +1,13 @@
 package com.example.turtle.ui.auth
 
+
 data class AuthResult(
     val data: UserData?,
     val emailErrorMessage: String?,
     val passwordErrorMessage: String?,
     val genericFormErrorMessage: String?,
 )
+
 
 data class UserData(
     val userId: String,
