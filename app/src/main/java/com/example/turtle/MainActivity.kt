b13acc2e48a1a91d45df9e3bb1d5c8db3f8c7a59
gabrymattioli <gabrymattioli@gmail.com>
2023-06-02 11:17:21 +0200

    MainActivity: refactoring, added support for navigateUp

--- app/src/main/java/com/example/turtle/MainActivity.kt
+++ app/src/main/java/com/example/turtle/MainActivity.kt
@@ -1,39 +1,20 @@
 package com.example.turtle
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.turtle.data.Bill
 import com.example.turtle.databinding.ActivityMainBinding
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+
+const val TAG = "MAIN"
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var billCollectionRef: DatabaseReference
-    private lateinit var auth: FirebaseAuth
-    private val TAG = "MAIN"
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,33 +22,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_profile,
-            )
-        )
+        appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
 
-        auth = Firebase.auth
+//        val navView: BottomNavigationView = binding.navView
 
-        val db = FirebaseDatabase.getInstance("https://turtle-2fdb6-default-rtdb.europe-west1.firebasedatabase.app/")
-        billCollectionRef = db.reference.child("Bill")
+//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_bills, R.id.navigation_profile,
+//            )
+//        )
+//
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
 
-        saveBill(Bill("prova", "bill prova"))
+//
+//        val user = intent.getParcelableExtra<UserData>("user")
+//        Toast.makeText(this, "${user?.username}", Toast.LENGTH_SHORT).show()
+//        val userBundle = bundleOf("user" to user)
+//        navController.setGraph(R.navigation.bottom_navigation, userBundle)
     }
 
-    private fun saveBill(bill: Bill) = CoroutineScope(Dispatchers.IO).launch {
-        try {
-            val newBillCollectionRef = billCollectionRef.push()
-            newBillCollectionRef.setValue(bill)
-        } catch (e: Exception) {
-            Log.e(TAG, e.message.toString())
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment_activity_main)
+            .navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
