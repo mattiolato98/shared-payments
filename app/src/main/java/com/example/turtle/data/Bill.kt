e009e3523830d395605e8d1e31cba3fe6b0c143f
gabrymattioli <gabrymattioli@gmail.com>
2023-06-04 12:40:36 +0200

    data, Bill: added createDateTime field, made description nullable

--- app/src/main/java/com/example/turtle/data/Bill.kt
+++ app/src/main/java/com/example/turtle/data/Bill.kt
@@ -1,10 +1,18 @@
 package com.example.turtle.data
 
 import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.ServerTimestamp
+import java.time.LocalDateTime
+import java.util.Calendar
+import java.util.Date
 
 data class Bill(
     @DocumentId
     var documentId: String? = null,
+ 
     var title: String = "",
-    var description: String = "",
+    var description: String? = null,
+
+    @ServerTimestamp
+    var createDateTime: Date? = null,
 )
