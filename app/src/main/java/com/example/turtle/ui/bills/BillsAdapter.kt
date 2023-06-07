6698687d3e113c917dfbd0bdb08ef366f3404777
gabrymattioli <gabrymattioli@gmail.com>
2023-06-07 11:21:44 +0200

    ui, BillsAdapter: added logic to fill profile pictures

--- app/src/main/java/com/example/turtle/ui/bills/BillsAdapter.kt
+++ app/src/main/java/com/example/turtle/ui/bills/BillsAdapter.kt
@@ -3,24 +3,42 @@ package com.example.turtle.ui.bills
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.turtle.R
 import com.example.turtle.data.Bill
+import com.google.android.material.card.MaterialCardView
+import com.squareup.picasso.Picasso
+
 
 class BillsAdapter(
-    private val listener: (Bill) -> Unit
+    private val onClickListener: (Bill) -> Unit,
+    private val onLongClickListener: (Bill) -> Boolean
 ): RecyclerView.Adapter<BillsAdapter.BillsViewHolder>() {
 
     inner class BillsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val cardView: MaterialCardView
         val billTitle: TextView
         val billDescription: TextView
+        val profileImages: List<ImageView>
+        val extraUsersText: TextView
 
         init {
+            cardView = itemView.findViewById(R.id.card_view)
             billTitle = itemView.findViewById(R.id.bill_title)
             billDescription = itemView.findViewById(R.id.bill_description)
+            profileImages = listOf(
+                itemView.findViewById(R.id.profile_image_01),
+                itemView.findViewById(R.id.profile_image_02),
+                itemView.findViewById(R.id.profile_image_03),
+                itemView.findViewById(R.id.profile_image_04),
+                itemView.findViewById(R.id.profile_image_05),
+            )
+            extraUsersText = itemView.findViewById(R.id.extra_users_text)
         }
     }
 
@@ -49,6 +67,7 @@ class BillsAdapter(
 
     override fun onBindViewHolder(holder: BillsViewHolder, position: Int) {
         val bill = differ.currentList[position]
+
         with(holder) {
             billTitle.text = bill.title
             if (bill.description.isNullOrEmpty())
@@ -56,7 +75,30 @@ class BillsAdapter(
             else {
                 billDescription.text = bill.description
             }
-            itemView.setOnClickListener { listener(bill) }
+
+            cardView.setOnClickListener { onClickListener(bill) }
+            cardView.setOnLongClickListener { onLongClickListener(bill) }
+        }
+
+        bill.users?.let { users ->
+            bill.users!!.forEachIndexed { index, profile ->
+                (holder.profileImages[index].parent as CardView).visibility = View.VISIBLE
+
+                Picasso.get().load(profile.profilePictureUrl)
+                    .resize(60, 60)
+                    .centerCrop()
+                    .into(holder.profileImages[index])
+
+                if (index == 4)
+                    return@forEachIndexed
+            }
+
+            (users.size - 5).coerceAtLeast(0).also { extra_users ->
+                if (extra_users > 0) {
+                    holder.extraUsersText.visibility = View.VISIBLE
+                    holder.extraUsersText.text = "+${extra_users}"
+                }
+            }
         }
     }
 
