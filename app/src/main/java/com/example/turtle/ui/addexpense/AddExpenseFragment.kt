238c83866effaf80f823faa8ba3de1c4550493b1
gabrymattioli <gabrymattioli@gmail.com>
2023-06-07 21:46:44 +0200

    ui, AddExpenseFragment: retrieved bill object and used to populate fields

--- app/src/main/java/com/example/turtle/ui/addexpense/AddExpenseFragment.kt
+++ app/src/main/java/com/example/turtle/ui/addexpense/AddExpenseFragment.kt
@@ -3,22 +3,33 @@ package com.example.turtle.ui.addexpense
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.core.view.size
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.example.turtle.data.Bill
 import com.example.turtle.databinding.FragmentAddExpenseBinding
+import com.example.turtle.ui.billdetail.TAG
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CancellationException
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
 
 class AddExpenseFragment: Fragment() {
-    val calendar = Calendar.getInstance()
 
     private var _binding: FragmentAddExpenseBinding? = null
     private val binding get() = _binding!!
+
+    private val calendar: Calendar = Calendar.getInstance()
+    private val args: AddExpenseFragmentArgs by navArgs()
+    private val billCollectionRef = Firebase.firestore.collection("bills")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,18 +42,10 @@ class AddExpenseFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        setUpUserPayingSpinner()
-        setUpDatePickerDialog()
-        setUpUsersPaidForListView()
+        setUp(args.billId)
     }
 
-    private fun setUpUserPayingSpinner() {
-        val users = listOf(
-            "Gabriele Mattioli",
-            "Lorenzo Baraldi",
-            "Fabrizio Garuti"
-        )
+    private fun setUpUserPayingSpinner(users: List<String>) {
         val dataAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, users)
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.userPayingSpinner.adapter = dataAdapter
@@ -74,18 +77,41 @@ class AddExpenseFragment: Fragment() {
         binding.fieldDate.setText(dateFormat.format(calendar.time))
     }
 
-    private fun setUpUsersPaidForListView() {
-        val list = listOf(
-            "Gabriele Mattioli",
-            "Lorenzo Baraldi",
-            "Fabrizio Garuti"
-        )
-        val dataAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_multiple_choice, list)
+    private fun setUpUsersPaidForListView(users: List<String>) {
+        val dataAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_multiple_choice, users)
         binding.usersPaidForList.choiceMode = ListView.CHOICE_MODE_MULTIPLE
         binding.usersPaidForList.adapter = dataAdapter
-        binding.usersPaidForList.setItemChecked(0, true)
-        binding.usersPaidForList.setItemChecked(1, true)
-        binding.usersPaidForList.setItemChecked(2, true)
+
+        for (i in users.indices) {
+            binding.usersPaidForList.setItemChecked(i, true)
+        }
+    }
+
+    private fun getBillUsers(bill: Bill): MutableList<String> {
+        val users = mutableListOf<String>()
+
+        for (user in bill.users!!) {
+            users.add(user.email.toString())
+        }
+
+        return users
+    }
+
+    private fun setUp(billId: String) {
+        try {
+            billCollectionRef.document(billId).get().addOnSuccessListener {
+                val bill = it.toObject(Bill::class.java)!!
+
+                val users = getBillUsers(bill)
+
+                setUpDatePickerDialog()
+                setUpUserPayingSpinner(users)
+                setUpUsersPaidForListView(users)
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, e.message.toString())
+            if (e is CancellationException) throw e
+        }
     }
 
     override fun onDestroyView() {
