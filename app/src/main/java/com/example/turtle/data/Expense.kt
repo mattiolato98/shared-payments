740a63e69f9fa738cfecd41d30cfa009382fc180
gabrymattioli <gabrymattioli@gmail.com>
2023-06-08 13:21:03 +0200

    data, Expense: updated fields date, amount

--- app/src/main/java/com/example/turtle/data/Expense.kt
+++ app/src/main/java/com/example/turtle/data/Expense.kt
@@ -1,18 +1,32 @@
 package com.example.turtle.data
 
+import com.example.turtle.utils.BigDecimalConverterDelegate
 import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.ServerTimestamp
 import java.math.BigDecimal
+import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 
-data class Expense (
+data class Expense(
     @DocumentId
     var documentId: String? = null,
 
     var title: String = "",
-    var amount: BigDecimal = BigDecimal(0.00),
-    var date: LocalDate = LocalDate.now(),
+
+    @get:Exclude
+    @set:Exclude
+    var bigDecimalAmount: BigDecimal = BigDecimal.ZERO,
+
+    @ServerTimestamp
+    var date: Date? = null,
 
     var userPayingId: String? = null,
     var userPaidForIds: List<String>? = null
-)
+) {
+    var amount: String by BigDecimalConverterDelegate(this::bigDecimalAmount)
+}
