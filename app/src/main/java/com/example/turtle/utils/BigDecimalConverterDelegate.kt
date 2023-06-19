0ec0c5415149ecead621a714440fb799e05bb8e0
gabrymattioli <gabrymattioli@gmail.com>
2023-06-19 11:28:30 +0200

    utils, BigDecimalConverterDelegate: added decimal formatter in getter

--- app/src/main/java/com/example/turtle/utils/BigDecimalConverterDelegate.kt
+++ app/src/main/java/com/example/turtle/utils/BigDecimalConverterDelegate.kt
@@ -1,12 +1,13 @@
 package com.example.turtle.utils
 
 import java.math.BigDecimal
+import java.math.RoundingMode
 import kotlin.reflect.KMutableProperty
 import kotlin.reflect.KProperty
 
 class BigDecimalConverterDelegate(private val bigDecimalProperty: KMutableProperty<BigDecimal>) {
     operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
-        return bigDecimalProperty.getter.call().toString()
+        return bigDecimalProperty.getter.call().setScale(2, RoundingMode.HALF_UP).toString()
     }
 
     operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
