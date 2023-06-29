601fab238562ea233813e3aed229ddc962d32c0d
gabrymattioli <gabrymattioli@gmail.com>
2023-06-29 17:26:56 +0200

    ui, BillDetailFragment: implemented ViewPager with TabLayout to navigate between fragments with bottom bar

--- app/src/main/java/com/example/turtle/ui/billdetail/BillDetailFragment.kt
+++ app/src/main/java/com/example/turtle/ui/billdetail/BillDetailFragment.kt
@@ -1,26 +1,22 @@
 package com.example.turtle.ui.billdetail
 
 import android.app.AlertDialog
-import android.graphics.Typeface
 import android.os.Bundle
-import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.lifecycle.Lifecycle
-import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.viewpager.widget.ViewPager
 import com.example.turtle.R
 import com.example.turtle.databinding.FragmentBillDetailBinding
 import com.google.android.material.snackbar.Snackbar
@@ -34,18 +30,10 @@ class BillDetailFragment: Fragment() {
 
     private val args: BillDetailFragmentArgs by navArgs()
 
-    private lateinit var expensesFragment: ExpensesFragment
-    private lateinit var balanceFragment: BalanceFragment
-
     private val billCollectionRef = Firebase.firestore.collection("bills")
 
-    private val tabs: List<LinearLayout> get() = binding.run {
-        listOf(binding.expensesNavigation, binding.balanceNavigation)
-    }
-    private val fragments: List<Fragment> get() = listOf(expensesFragment, balanceFragment)
-
-    private var selectedIndex = 0
-
+    private lateinit var billDetailPagerAdapter: BillDetailPagerAdapter
+    private lateinit var viewPager: ViewPager
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -53,84 +41,13 @@ class BillDetailFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentBillDetailBinding.inflate(inflater, container, false)
-
-        if (savedInstanceState == null) {
-            expensesFragment = ExpensesFragment()
-            balanceFragment = BalanceFragment()
-
-            val bundle = bundleOf(Pair("billId", args.billId))
-            expensesFragment.arguments = bundle
-            balanceFragment.arguments = bundle
-
-            childFragmentManager.beginTransaction()
-                .add(R.id.container, expensesFragment, "expensesFragment")
-                .add(R.id.container, balanceFragment, "balanceFragment")
-                .selectFragment(selectedIndex)
-                .commit()
-
-        } else {
-            selectedIndex = savedInstanceState.getInt("selectedIndex", 0)
-
-            expensesFragment = childFragmentManager.findFragmentByTag("expensesFragment") as ExpensesFragment
-            balanceFragment = childFragmentManager.findFragmentByTag("balanceFragment") as BalanceFragment
-        }
-
         setupMenuProvider()
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        setupTabSelectedState(selectedIndex)
-        tabs.forEachIndexed { index, textView ->
-            textView.setOnClickListener { selectFragment(index) }
-        }
-    }
-
-    private fun FragmentTransaction.selectFragment(selectedIndex: Int?): FragmentTransaction {
-        fragments.forEachIndexed { index, fragment ->
-            if (index == selectedIndex) attach(fragment) else detach(fragment)
-        }
-        return this
-    }
-
-    private fun selectFragment(index: Int) {
-        selectedIndex = index
-        setupTabSelectedState(index)
-        childFragmentManager.beginTransaction().selectFragment(index).commit()
-    }
-
-    private fun setupTabSelectedState(selectedIndex: Int) {
-        val theme = requireContext().theme
-        val primaryColor = TypedValue()
-        val defaultTextColor = TypedValue()
-        theme.resolveAttribute(androidx.appcompat.R.attr.colorPrimary, primaryColor, true)
-        theme.resolveAttribute(android.R.attr.textColorPrimary, defaultTextColor, true)
-
-        tabs.forEachIndexed { index, layout ->
-            val icon = (layout.getChildAt(0) as ImageView)
-            val textView = (layout.getChildAt(1) as TextView)
-
-            when (index) {
-                selectedIndex -> {
-                    textView.typeface = Typeface.DEFAULT_BOLD
-                    textView.setTextColor(primaryColor.data)
-                    icon.setColorFilter(primaryColor.data)
-                }
-                else -> {
-                    textView.typeface = Typeface.DEFAULT
-                    textView.setTextColor(defaultTextColor.data)
-                    icon.setColorFilter(defaultTextColor.data)
-                }
-            }
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt("selectedIndex", selectedIndex)
+        setUpTabLayout()
     }
 
     private fun navigateToEditBill() {
@@ -138,11 +55,6 @@ class BillDetailFragment: Fragment() {
             args.billId,
             resources.getString(R.string.edit_bill)
         )
-        navigateToDirection(action)
-    }
-
-    private fun navigateToDirection(action: NavDirections) {
-        childFragmentManager.beginTransaction().selectFragment(null).commit()
         findNavController().navigate(action)
     }
 
@@ -194,9 +106,43 @@ class BillDetailFragment: Fragment() {
         )
     }
 
+    private fun setUpTabLayout() {
+        billDetailPagerAdapter = BillDetailPagerAdapter(childFragmentManager, args.billId)
+        viewPager = binding.pager
+        viewPager.adapter = billDetailPagerAdapter
+        binding.tabLayout.setupWithViewPager(viewPager)
+
+        binding.tabLayout.getTabAt(0)?.setIcon(R.drawable.receipt_long_material)
+        binding.tabLayout.getTabAt(1)?.setIcon(R.drawable.account_balance_material)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
+}
+
+class BillDetailPagerAdapter(fm: FragmentManager, private val billId: String): FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    private val expensesFragment = ExpensesFragment()
+    private val balanceFragment = BalanceFragment()
+    private val fragments: List<Fragment> get() = listOf(expensesFragment, balanceFragment)
+
+    override fun getCount(): Int = 2
+
+    override fun getItem(position: Int): Fragment {
+        val fragment = fragments[position]
+        val bundle = bundleOf(Pair("billId", billId))
+        fragment.arguments = bundle
+
+        return fragment
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return if (fragments[position] == expensesFragment)
+            "Expenses"
+        else
+            "Balance"
+    }
 }
