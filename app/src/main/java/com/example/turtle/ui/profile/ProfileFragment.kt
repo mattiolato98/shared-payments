aff0dac3ee6a4b512044663b45e93eb2568bddd2
gabrymattioli <gabrymattioli@gmail.com>
2023-07-01 11:47:30 +0200

    Implemented FragmentProfile

--- app/src/main/java/com/example/turtle/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/turtle/ui/profile/ProfileFragment.kt
@@ -1,74 +1,83 @@
 package com.example.turtle.ui.profile
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.OnClickListener
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.turtle.AuthActivity
+import com.example.turtle.R
+import com.example.turtle.data.Profile
 import com.example.turtle.databinding.FragmentProfileBinding
-import com.firebase.ui.auth.AuthUI
-import com.google.firebase.auth.FirebaseUser
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
-class ProfileFragment : Fragment(), OnClickListener {
+class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
-    private lateinit var signOutButton: Button
-    private lateinit var context: Context
-    private var user: FirebaseUser? = null
+
+    private val auth = Firebase.auth
+    private lateinit var oneTapClient: SignInClient
+
+    private val profileCollectionRef = Firebase.firestore.collection("profiles")
+    private lateinit var profile: Profile
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val profileViewModel: ProfileViewModel by viewModels()
-
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
-
-        signOutButton = binding.signOutButton
-        signOutButton.setOnClickListener(this)
-
-        context = container!!.context
-
-        val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-
-        user = Firebase.auth.currentUser
-
-        return root
+        return binding.root
     }
 
-    override fun onClick(v: View) {
-        when (v.id) {
-            binding.signOutButton.id -> onSignOutClicked()
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        oneTapClient = Identity.getSignInClient(requireActivity())
+
+        setUpProfile()
+
+        binding.signOutButton.setOnClickListener { signOut() }
     }
 
-    private fun onSignOutClicked() {
-        AuthUI.getInstance()
-            .signOut(context)
-            .addOnCompleteListener {
-                val i = Intent(requireContext(), AuthActivity::class.java)
-                startActivity(i)
-                requireActivity().finish()
-            }
+    private fun setUpProfile() = viewLifecycleOwner.lifecycleScope.launch {
+        val profileDoc = profileCollectionRef.whereEqualTo("userId", auth.currentUser?.uid).get().await().first()
+        profile = profileDoc.toObject(Profile::class.java)
+
+        if (profile.profilePictureUrl != null) {
+            Picasso.get()
+                .load(profile.profilePictureUrl!!.replace("96", "400").toUri())
+                .into(binding.profileImage)
+        } else {
+            binding.profileImage.setImageResource(R.drawable.profile_material)
+        }
+
+        binding.displayName.text = profile.displayName
+        binding.email.text = profile.email
+    }
+
+    private fun signOut() = viewLifecycleOwner.lifecycleScope.launch {
+        oneTapClient.signOut().await()
+        auth.signOut()
+
+        startActivityAuth()
+    }
+
+    private fun startActivityAuth() {
+        Intent(requireContext(), AuthActivity::class.java).also {
+            startActivity(it)
+        }
+        requireActivity().finish()
     }
 
     override fun onDestroyView() {
