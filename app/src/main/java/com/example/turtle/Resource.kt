840f65951cf6ac318319611f86a03af453a2b847
gabrymattioli <gabrymattioli@gmail.com>
2023-07-06 16:13:12 +0200

    Added Resource

---
+++ app/src/main/java/com/example/turtle/Resource.kt
@@ -0,0 +1,9 @@
+package com.example.turtle
+
+sealed class Resource<T>(
+    val data: T?,
+    val message: String? = null,
+) {
+    class Success<T>(data: T): Resource<T>(data)
+    class Error<T>(message: String, data: T? = null): Resource<T>(data, message)
+}
