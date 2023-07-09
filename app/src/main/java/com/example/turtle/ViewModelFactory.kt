e3cdb9d840c1f0c560e222c4f92ce212ef2e1484
gabrymattioli <gabrymattioli@gmail.com>
2023-07-09 13:39:18 +0200

    ViewModelFactory: updated with new viewModel

--- app/src/main/java/com/example/turtle/ViewModelFactory.kt
+++ app/src/main/java/com/example/turtle/ViewModelFactory.kt
@@ -3,6 +3,7 @@ package com.example.turtle
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.turtle.ui.addeditbill.AddEditBillViewModel
 import com.example.turtle.ui.billdetail.BillDetailViewModel
 import com.example.turtle.ui.bills.BillsViewModel
 import com.example.turtle.ui.expensedetail.ExpenseDetailViewModel
@@ -15,7 +16,9 @@ class ViewModelFactory(
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        val billRepository = (application as TurtleApplication).billRepository
+        val app = application as TurtleApplication
+        val authRepository = app.authRepository
+        val billRepository = app.billRepository
 
         return with(modelClass) {
             when {
@@ -23,11 +26,18 @@ class ViewModelFactory(
                     BillDetailViewModel(billRepository, billId!!) as T
                 }
                 isAssignableFrom(BillsViewModel::class.java) -> {
-                    val userId = application.userId!!
-                    BillsViewModel(billRepository, userId) as T
+                    BillsViewModel(billRepository, app.userId!!) as T
                 }
                 isAssignableFrom(ExpenseDetailViewModel::class.java) -> {
                     ExpenseDetailViewModel(billRepository, billId!!, expenseId!!) as T
+                }
+                isAssignableFrom(AddEditBillViewModel::class.java) -> {
+                    AddEditBillViewModel(
+                        authRepository, billRepository,
+                        app.userId!!,
+                        billId,
+                        app.userEmail!!
+                    ) as T
                 }
                 else -> {
                     throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
