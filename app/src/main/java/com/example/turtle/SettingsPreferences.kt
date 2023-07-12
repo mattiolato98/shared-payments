72a22c7f02b137f479321e3a6d73138e3f996d05
gabrymattioli <gabrymattioli@gmail.com>
2023-07-12 11:12:52 +0200

    SettingsPreferences: added clear method

--- app/src/main/java/com/example/turtle/SettingsPreferences.kt
+++ app/src/main/java/com/example/turtle/SettingsPreferences.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
-import com.example.turtle.data.Profile
 import kotlinx.coroutines.flow.map
 
 class SettingsPreferences(private val context: Context) {
@@ -27,5 +26,9 @@ class SettingsPreferences(private val context: Context) {
             it[username] = uname
             it[email] = userEmail
         }
+    }
+
+    suspend fun clearPreferences() {
+        context.dataStore.edit { it.clear() }
     }
 }
