f352b26a4f7fdad41056ba3ad6aede53e2238493
gabrymattioli <gabrymattioli@gmail.com>
2023-07-18 11:13:56 +0200

    TurtleApplication: added AlarmScheduler and notification related stuff

--- app/src/main/java/com/example/turtle/TurtleApplication.kt
+++ app/src/main/java/com/example/turtle/TurtleApplication.kt
@@ -1,6 +1,10 @@
 package com.example.turtle
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import com.example.turtle.data.AuthRepository
 import com.example.turtle.data.BillRepository
 
@@ -16,10 +20,14 @@ class TurtleApplication: Application() {
     var userEmail: String? = null
         private set
 
+    var alarmScheduler: AlarmScheduler? = null
+        private set
+
     override fun onCreate() {
         super.onCreate()
         billRepository = BillRepository()
         authRepository = AuthRepository()
+        createNotificationChannel()
     }
 
     fun setUserId(userId: String?) {
@@ -28,5 +36,26 @@ class TurtleApplication: Application() {
 
     fun setUserEmail(email: String?) {
         this.userEmail = email
+    }
+
+    fun setAlarmScheduler(alarmScheduler: AlarmScheduler) {
+        this.alarmScheduler = alarmScheduler
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Debts reminder"
+            val descriptionText = "This notification channel reminds you your pending debts, once per day"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("debts", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
